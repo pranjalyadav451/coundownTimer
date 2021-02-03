a8f9abd27179fea7810991d1a9114a7ae23c6de8
Pranjal Yadav <2019uec1008@mnit.ac.in>
2021-02-03 18:55:19 +0530

    my first website

---
+++ app.js
@@ -0,0 +1,23 @@
+const days = document.querySelector(".days");
+const hours = document.querySelector(".hours");
+const minutes = document.querySelector(".minutes");
+const seconds = document.querySelector(".seconds");
+
+  
+function countdown()
+{
+    const date = new Date(2021,1,22);
+    const current = new Date();
+    let sec = Math.floor((date - current)/1000);
+    let min = Math.floor(sec/60);
+    let hr = Math.floor(min/60);
+    let day = Math.floor(hr/24);
+    
+    days.innerHTML = day;
+    hours.innerHTML = hr%24;
+    minutes.innerHTML = min%60;
+    seconds.innerHTML = sec%60;
+}
+countdown();
+setInterval(countdown,1000);
+
