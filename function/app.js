a04526dba7e1821ab9470c04b5f38f4fdbd211db
Pranjal Yadav <2019uec1008@mnit.ac.in>
2021-02-07 18:22:49 +0530

    tinkering with animations

--- function/app.js
+++ function/app.js
@@ -6,21 +6,20 @@ const currentDate = document.querySelector(".currentDate")
 const dateTitle = new Date();
 currentDate.innerHTML += dateTitle.toDateString("en-us");
 
-  
-function countdown()
-{
-    const date = new Date(2021,1,22);
+function countdown() {
+
+    const date = new Date(2021, 1, 22);
     const current = new Date();
-    let sec = Math.floor((date - current)/1000);
-    let min = Math.floor(sec/60);
-    let hr = Math.floor(min/60);
-    let day = Math.floor(hr/24);
-    
+    let sec = Math.floor((date - current) / 1000);
+    let min = Math.floor(sec / 60);
+    let hr = Math.floor(min / 60);
+    let day = Math.floor(hr / 24);
+
     days.innerHTML = day;
-    hours.innerHTML = hr%24;
-    minutes.innerHTML = min%60;
-    seconds.innerHTML = sec%60;
+    hours.innerHTML = hr % 24;
+    minutes.innerHTML = min % 60;
+    seconds.innerHTML = sec % 60;
 }
 countdown();
-setInterval(countdown,1000);
+setInterval(countdown, 1000);
 
